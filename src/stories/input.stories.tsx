2e85cc78c16e00e58e9e6fcb969209741883aa9f
wostrau <a.ostrovtsov@gmail.com>
2022-08-10 23:17:42 +0300

    done up to video lesson number 12th <uncontrolled input topic>

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,11 +1,11 @@
-import {ChangeEvent, useRef, useState} from 'react';
-
+import React, {ChangeEvent, useRef, useState} from 'react';
+import {action} from '@storybook/addon-actions';
 
 export default {
     title: 'input'
 };
 
-export const UncontrolledInput = () => <input/>;
+//export const UncontrolledInput = () => <input/>;
 
 export const TrackValueOfUncontrolledInput = () = {
     const [value, setValue] = useState('');
@@ -13,25 +13,30 @@ export const TrackValueOfUncontrolledInput = () = {
     const onChange = (event: ChangeEvent<HTMLInputElement>) => {
         const actualValue = event.currentTarget.value;
         setValue(actualValue);
-
-        return <><input value={value} onChange={onChange}/> - {value}</>;
     };
 
-    export const GetValueOfUncontrolledInputByButtonPress = () => {
-        const [value, setValue] = useState('');
-        const inputRef = useRef<HTMLInputElement>(null);
+    return <div>
+        <input value={value} onChange={onChange} />
+        - {value}
+    </div>;
 
-        const save = () => {
-            const el = inputRef.current as HTMLInputElement;
-            setValue(el.value);
-        }
-        return <>
-            <input ref={inputRef}/>
-            <button onClick={save}>save</button>
-            - actual value: {value}
-        </>;
-    };
+};
 
-    export const ControlledInputWithFixedValue = () => {
-        <input value={'sanya\'s incubator'}/>
-    };
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('');
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement;
+        setValue(el.value);
+    }
+    return <>
+        <input ref={inputRef}/>
+        <button onClick={save}>save</button>
+        - actual value: {value}
+    </>;
+};
+
+export const ControlledInputWithFixedValue = () => {
+    <input value={'sanya\'s incubator'}/>
+};
