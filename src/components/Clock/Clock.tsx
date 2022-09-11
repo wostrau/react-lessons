c919c064a0619089da96dc8d4241fe5b82c5f7ab
wostrau <a.ostrovtsov@gmail.com>
2022-09-11 16:26:55 +0300

    DONE video-lesson #24 / lesson topic: analog clock

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,8 +1,14 @@
 import React, {useEffect, useState} from 'react';
+import {DigitalClockView} from './DigitalClockView';
+import {AnalogClockView} from './AnalogClockView';
 
-type ClockPropsType = {}
+export type ClockViewPropsType = {
+    date: Date
+};
 
-const get2digitsString = (num: number) => num < 10 ? '0' + num : num;
+type ClockPropsType = {
+    mode?: 'digital' | 'analog'
+}
 
 export const Clock: React.FC<ClockPropsType> = (props) => {
     const [date, setDate] = useState(new Date());
@@ -16,13 +22,18 @@ export const Clock: React.FC<ClockPropsType> = (props) => {
         };
     }, [])
 
+    let view;
+
+    switch (props.mode) {
+        case 'analog':
+            view = <AnalogClockView date={date}/>
+            break;
+        case 'digital':
+        default:
+            view = <DigitalClockView date={date}/>
+    }
+
     return <div>
-        <span>{get2digitsString(date.getHours())}</span>
-        :
-        <span>{get2digitsString(date.getMinutes())}</span>
-        :
-        <span>{get2digitsString(date.getSeconds())}</span>
-        :
+        {view}
     </div>
 };
-
